bead7f16710cfc12604a549be14f4a3f8aeb0c35
praz <sachabouillez@gmail.com>
2025-07-18 18:56:34 +0200

    feat: metatada + icon fix + sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://praz.garden',
   build: {
     format: 'file'
   },
+  integrations: [
+    sitemap()
+  ]
 })
