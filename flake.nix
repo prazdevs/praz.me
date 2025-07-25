dec485e9697072357df81e5aa19989f2fddf6db9
praz <sachabouillez@gmail.com>
2025-07-25 02:14:29 +0200

    chore: cleaner flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
+    { nixpkgs, self, ... }:
+    let
+      supportedSystems = [ "aarch64-darwin" ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
             zola
           ];
         };
-      }
-    );
+      });
+    };
 }
